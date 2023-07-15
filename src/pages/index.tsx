9fc649cae023c3470beddeb3c7eac613a1a7426c
Nikita <zeninnikita96@gmail.com>
2023-07-15 23:44:43 +0200

    working on react query api call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+
 export default function Home() {
-  const callAPI = async () => {
-    try {
-      const res = await fetch(`/api/hello`);
-      const data = await res.json();
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  const helloQuery = useQuery({
+    queryKey: ["hello"],
+    queryFn: async () => {
+      const response = await fetch("/api/hello");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    },
+  });
+
+  console.log("blah");
+  // const callAPI = async () => {
+  //   try {
+  //     const res = await fetch(`/api/hello`);
+  //     const data = await res.json();
+  //     console.log(data);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
   return (
     <div>
-      <main>
-        <button onClick={callAPI}>Make API Call</button>
-      </main>
+      <main>{/* <button onClick={callAPI}>Make API Call</button> */}</main>
+      {/* <div>{helloQuery.data}</div> */}
     </div>
   );
 }
