f9ee5c7db2cd4fa5749f8b84c8c94857a6e154fc
Nikita <zeninnikita96@gmail.com>
2023-07-16 23:19:56 +0200

    finish work on react query request

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,8 +11,9 @@ import {
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return;
-  <QueryClientProvider client={queryClient}>
-    <Component {...pageProps} />
-  </QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
+const callAPI = async () => {
+  try {
+    const res = await fetch(`/api/hello`);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
 
 export default function Home() {
   const helloQuery = useQuery({
     queryKey: ["hello"],
-    queryFn: async () => {
-      const response = await fetch("/api/hello");
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    },
+    queryFn: callAPI,
+    staleTime: 0,
   });
 
-  console.log("blah");
-  // const callAPI = async () => {
-  //   try {
-  //     const res = await fetch(`/api/hello`);
-  //     const data = await res.json();
-  //     console.log(data);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  return (
-    <div>
-      <main>{/* <button onClick={callAPI}>Make API Call</button> */}</main>
-      {/* <div>{helloQuery.data}</div> */}
-    </div>
-  );
+  console.log(helloQuery.data);
+  if (helloQuery.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (helloQuery.isError) {
+    return <div>An error occurred</div>;
+  }
+
+  return <div>Home</div>;
 }
